5eb4dc8ee899e2722f1bebc0a5be8807ff0283f0
James D. Hasselman <james.d.hasselman@gmail.com>
2022-10-13 11:27:24 -0400

    Checkpoint

--- Hasselman.Backsplice.Spreadsheet.Excel/ExcelRow.cs
+++ Hasselman.Backsplice.Spreadsheet.Excel/ExcelRow.cs
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 using DocumentFormat.OpenXml.Spreadsheet;
+using System.Collections;
+using System.Collections.Specialized;
 
 #if DEBUG
 using System.Runtime.CompilerServices;
@@ -13,26 +15,64 @@ namespace Hasselman.Backsplice.Spreadsheet.Excel
     public class ExcelRow : IRow
     {
         internal Row row;
-        private IList<ICell> cells;
+        private ObservableList<ExcelCell> cells;
 
         public ExcelRow()
         {
             row = new Row();
-            cells = new List<ICell>();
+            cells = new ObservableList<ExcelCell>();
+            cells.ItemAdded += Cells_ItemAdded;
+            cells.ItemInserted += Cells_ItemInserted;
+            cells.ItemRemoved += Cells_ItemRemoved;
+            cells.ItemRemovedAt += Cells_ItemRemovedAt;
+            cells.ItemUpdated += Cells_ItemUpdated;
         }
 
         internal ExcelRow(Row row)
         {
             this.row = row;
-            if (row.ChildElements.Count == 0)
+            if (row.ChildElements.Count > 0)
             {
-                cells = new List<ICell>();
-            } else
-            {
-                var cells = from cell in row.ChildElements as IEnumerable<Cell>
-                            select new ExcelCell(cell);
-                this.cells = (IList<ICell>)cells.ToList();
+                var excelCells = from cell in row.ChildElements as IEnumerable<Cell>
+                                 select new ExcelCell(cell);
+                cells = new ObservableList<ExcelCell>(excelCells);
             }
+            else
+            {
+                cells = new ObservableList<ExcelCell>();
+            }
+            cells.ItemAdded += Cells_ItemAdded;
+            cells.ItemInserted += Cells_ItemInserted;
+            cells.ItemRemoved += Cells_ItemRemoved;
+            cells.ItemRemovedAt += Cells_ItemRemovedAt;
+            cells.ItemUpdated += Cells_ItemUpdated;
+        }
+
+        private void Cells_ItemRemovedAt(object? sender, int index)
+        {
+            var item = row.Elements<Cell>().ElementAt(index);
+            row.RemoveChild(item);
+        }
+
+        private void Cells_ItemRemoved(object? sender, ExcelCell item)
+        {
+            row.RemoveChild(item.cell);
+        }
+
+        private void Cells_ItemInserted(object? sender, int index, ExcelCell item)
+        {
+            row.InsertAt(item.cell, index);
+        }
+
+        private void Cells_ItemAdded(object? sender, ExcelCell item)
+        {
+            row.AddChild(item.cell);
+        }
+
+        private void Cells_ItemUpdated(object? sender, int index, ExcelCell item)
+        {
+            var oldItem = row.ElementAt(index) as Cell;
+            row.ReplaceChild<Cell>(item.cell, oldItem);
         }
 
         public double? Height
@@ -54,17 +94,18 @@ namespace Hasselman.Backsplice.Spreadsheet.Excel
         }
         public IList<ICell> Cells
         {
-            get => cells;
+            get
+            {
+                var cells = new List<ICell>(this.cells);
+                return cells;
+            }
             set
             {
-                row.RemoveAllChildren<Row>();
-                var excelCells = value as IList<ExcelCell>;
-                if (excelCells != null)
+                cells.Clear();
+                // TODO fix
+                foreach (var cell in value)
                 {
-                    foreach (var cell in excelCells)
-                    {
-                        row.AppendChild(cell.cell);
-                    }
+                    cells.Add(new ExcelCell());
                 }
             }
         }
